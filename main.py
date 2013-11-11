a0223c04d1255859982200a12fcb1c8f9005e008
Orest Voloshchuk <orest.v@gmail.com>
2013-11-11 22:37:40 +0200

    Added import code to main.

--- main.py
+++ main.py
@@ -3,13 +3,15 @@
 __author__ = 'seth'
 
 from argparse import ArgumentParser
+from geometry import importutils
+from geometry import geometryutils
 
 def parse_options():
     parser = ArgumentParser(description=u'Обчислення параметрів моделі.')
 
     parser.add_argument('input_file', type=str,
                         help=u'Повний шлях до .obj-файла моделі')
-    parser.add_argument('-o', type=str,
+    parser.add_argument('-o', '--output_file', type=str,
                         help=u'Шлях до файла, куди буде збережно результат.')
     parser.add_argument('-op', '--observation_point',
                         type=float, nargs=3, required=True,
@@ -21,10 +23,15 @@ def parse_options():
                         help=u'Вектор спостереження')
 
     args = parser.parse_args()
-    print args
+    return args
 
 def main():
-    parse_options()
+    args = parse_options()
+    quads = importutils.import_obj(args.input_file)
+    for quad in quads:
+        if not geometryutils.is_rectangle(quad):
+            print u'Не квадрат: %s' % quad
+
 
 if __name__ == '__main__':
     main()
