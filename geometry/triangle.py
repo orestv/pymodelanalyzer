e909039fc2860c3674c5cee8c4c89e1e429e3480
Orest Voloshchuk <orest.v@gmail.com>
2013-12-14 15:56:19 +0200

    Added triangle class.

--- geometry/triangle.py
+++ geometry/triangle.py
@@ -1,1 +1,10 @@
-__author__ = 'seth'
+class RightTriangle(object):
+    def __init__(self, vertex, leg_1, leg_2):
+        self.vertex = vertex
+        self.leg_1 = leg_1
+        self.leg_2 = leg_2
+
+    @property
+    def normale(self):
+        product = self.leg_1.vector_product(self.leg_2)
+        return product.get_normalized()
